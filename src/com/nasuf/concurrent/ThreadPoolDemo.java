42932404f19b01adda09c1d171890b9d8ce3a0d7
pocketContributor <842100455@qq.com>
2020-03-07 11:45:15 +0800

    init

---
+++ src/com/nasuf/concurrent/ThreadPoolDemo.java
@@ -0,0 +1,29 @@
+package com.nasuf.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo {
+
+    public static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis() + ":Thread ID:" + Thread.currentThread().getId());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyTask myTask = new MyTask();
+        ExecutorService es = Executors.newCachedThreadPool();
+        for (int i=0; i<10; i++) {
+            es.submit(myTask);
+        }
+    }
+
+}
